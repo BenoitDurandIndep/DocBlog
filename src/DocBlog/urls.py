7becd9385ac7762772b8fdf871a0e4671ba5b9cb
Benoit Durand <benoit.durand.indep@gmail.com>
2022-04-29 17:07:19 +0700

    dev blog / urls

--- src/DocBlog/urls.py
+++ src/DocBlog/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.defaults import server_error
+from .views import index
+#from blog.views import blog_index
 
 urlpatterns = [
+    path('',index, name="index"),
+    path('blog/',include("blog.urls")),
     path('admin/', admin.site.urls),
-    path('bonjour/',server_error)
+    path('error/',server_error)
+    
 ]
